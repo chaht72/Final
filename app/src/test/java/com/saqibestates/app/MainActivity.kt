5eb5bc76880b8c3c9407ae1a86b71a835691e5f4
chaht72 <chaht@zainmotors.com.pk>
2023-05-28 15:36:38 +0000

    Code Synced by DhiWise

---
+++ app/src/test/java/com/saqibestates/app/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.saqibestates.app
+
+import com.saqibestates.app.appcomponents.base.BaseActivity
+import com.saqibestates.app.databinding.LayoutProgressDialogBinding
+
+class MainActivity : BaseActivity<LayoutProgressDialogBinding>(R.layout.layout_progress_dialog) {
+
+    override fun onInitialized() {
+
+    }
+
+    override fun setUpClicks() {
+
+    }
+}
